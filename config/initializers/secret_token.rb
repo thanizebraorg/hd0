6d1486a311f1cf0023e19570a1923d2b97bc7668
Thani <dvh638@zebra.com>
2015-10-23 10:00:44 +0530

    0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hd0::Application.config.secret_token = 'aaf631dcaadcc2984383bda1cc5df82bd1d3fed328a00f5d869aa95a51a87b1682ed6822b8fbf83e2560823998645f00b882e24f4b2743a6e015472394b33129'
